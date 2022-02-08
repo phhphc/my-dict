861f2138eea0f84e7c02f41e383af3209930f3a7
Pham Phuoc <phhphc@gmail.com>
2022-02-08 15:21:03 +0700

    u

--- pages/api/user/[word].js
+++ pages/api/user/[word].js
@@ -1,16 +1,8 @@
-import { deleteUserWord, getWord } from "../../../database/database";
+import { deleteUserWord } from "../../../database/database";
 
 export default async function handler(req, res) {
     if (req.method == 'DELETE') {
         res.status(200).send(await deleteUserWord(req.query.word))
         
-    } else if (req.method == 'GET') {
-        const wordData = await getWord(req.query.word)
-
-        if (wordData == null) {
-            res.status(404).send()
-        } else {
-            res.status(200).send(wordData)
-        }
     }
 }
